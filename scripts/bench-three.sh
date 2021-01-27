41c50a4f4266c14ae2d842a84c9cacacae71cbfd
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-01-27 08:23:47 -0300

    fix(bench-three): replace $$ with $ on for iterator
    As inherited from official esbuild Makefile, the $$ variable actually
    refers to the process ID (PID), and it's resulting concatenation with $i
    end up generating some weird undesired output, as stated by Evan Wallace
    (esbuild maintainer) the project benchmark scripts aren't
    cross-platform, so maybe it's a specific zsh/macOS shell syntax

--- scripts/bench-three.sh
+++ scripts/bench-three.sh
@@ -2,6 +2,6 @@
 
 mkdir -p bench/three/src
 echo > bench/three/src/entry.js
-for i in 1 2 3 4 5 6 7 8 9 10; do test -d "bench/three/src/copy$$i" || cp -r github/three/src "bench/three/src/copy$$i"; done
-for i in 1 2 3 4 5 6 7 8 9 10; do echo "import * as copy$$i from './copy$$i/Three.js'; export {copy$$i}" >> bench/three/src/entry.js; done
+for i in 1 2 3 4 5 6 7 8 9 10; do test -d "bench/three/src/copy$i" || cp -r github/three/src "bench/three/src/copy$i"; done
+for i in 1 2 3 4 5 6 7 8 9 10; do echo "import * as copy$i from './copy$i/Three.js'; export {copy$i}" >> bench/three/src/entry.js; done
 echo 'Line count:' && find bench/three/src -name '*.js' | xargs wc -l | tail -n 1
