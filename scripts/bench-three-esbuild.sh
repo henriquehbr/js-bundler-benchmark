23e90faa683168244574e68d77e823077774e8b9
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-01-27 09:47:23 -0300

    fix: remove backslash right before esbuild command

--- scripts/bench-three-esbuild.sh
+++ scripts/bench-three-esbuild.sh
@@ -3,14 +3,12 @@
 # Clears esbuild output before running tests
 rm -rf bench/three/esbuild
 
-time -p
-  esbuild \
+time -p esbuild \
   --bundle \
   --summary \
   --global-name=THREE \
   --sourcemap \
-  --minify \
-  bench/three/src/entry.js \
+  --minify bench/three/src/entry.js \
   --outfile=bench/three/esbuild/entry.esbuild.js
 
 du -h bench/three/esbuild/entry.esbuild.js*
